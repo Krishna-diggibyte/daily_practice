2391f851ec376f681291c65544401b95a02fa8c6
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-05-18 01:31:04 +0530

    destructor in python

---
+++ destructor_python.py
@@ -0,0 +1,10 @@
+class MyClass:
+    def __init__(self):
+        print("Constructor called")
+
+    def __del__(self):
+        print("Destructor called")
+
+
+obj = MyClass()
+del obj  # Destructor called when object is deleted
